f25b0ad2c1c96ef243aacdf450fa1e61ef3e221e
anthonymendieta1 <51386847+anthonymendieta1@users.noreply.github.com>
2020-08-21 14:56:44 -0500

    primer envio.... segundo envio....

--- src/java/jave/MenuView.java
+++ src/java/jave/MenuView.java
@@ -1,6 +1,8 @@
-
+package jave;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import org.primefaces.model.menu.DefaultMenuItem;
 import org.primefaces.model.menu.DefaultMenuModel;
